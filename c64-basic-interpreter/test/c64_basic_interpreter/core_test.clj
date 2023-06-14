a850ff1010a8964fa3013c5603d93b419550dc0b
amaruDuran <amaruduran@gmail.com>
2023-06-14 16:38:55 -0300

    agregando esqueleto del tp

---
+++ c64-basic-interpreter/test/c64_basic_interpreter/core_test.clj
@@ -0,0 +1,7 @@
+(ns c64-basic-interpreter.core-test
+  (:require [clojure.test :refer :all]
+            [c64-basic-interpreter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
